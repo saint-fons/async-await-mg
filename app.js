ea68a560aac41d529233be6527f18962d938420e
IvanZhuravlev <i.zhuravlev.work@gmail.com>
2020-10-19 16:14:23 +0300

    version2 blur and reblur 2sec. after

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-console.log("hello")
-window.onload = pageLoaderHandler;
-function pageLoaderHandler() {
-    alert("Iam alive!");
-}
-
-
-function init() {
+window.onload = function() {
     let images = document.getElementsByTagName("img");
     for (let i = 0; i < images.length; i++) {
         images[i].onclick = showAnswer;
     }
-}
-
+};
 function showAnswer(eventObj) {
     let image = eventObj.target;
     let name = image.id;
     name = name + ".jpg";
     image.src = name;
+    setTimeout(reblur, 2000, image);
 }
-
-
-
-window.onload = init;
+function reblur(image) {
+    let name = image.id;
+    name = name + "blur.jpg";
+    image.src = name;
+}
